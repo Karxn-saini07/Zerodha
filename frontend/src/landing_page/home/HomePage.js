0f051d1d17fa4a4dc7d1a52d501efdc57c657ee3
Karan <karxnsaini07@gmail.com>
2026-02-04 23:27:39 +0530

    update file

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Hero from './Hero';
 import Awards from './Awards';
-import Stats from './Stats';
-import Pricing from './Pricing';
-import Education from './Education';
+// import Stats from './Stats';
+// import Pricing from './Pricing';
+// import Education from './Education';
 
-import OpenAccount from '../OpenAccount';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
+// import OpenAccount from '../OpenAccount';
+// import Navbar from '../Navbar';
+// import Footer from '../Footer';
 
 function HomePage() {
     return (
