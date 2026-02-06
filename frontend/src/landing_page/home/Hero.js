d96c968857ae81d0651602b0bc7c5d99d70649e8
Karan <karxnsaini07@gmail.com>
2026-02-07 02:25:30 +0530

    update in the file

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Hero() {
     return (
-        <h1>Hero</h1>
+        <div className='container'>
+            <div className='row'>
+                <img src='media/images/homeHero.png' alt='Hero Image'/>
+                <h1>Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button>Signup Now</button>
+            </div>
+        </div>
       );
 }
 
