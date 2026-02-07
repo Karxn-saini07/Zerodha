dbc783c43f539ca2b4b4a6a765706c88d751fd5c
Karan <karxnsaini07@gmail.com>
2026-02-07 21:58:05 +0530

    add some more code

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Hero() {
     return (
-        <div className='container'>
-            <div className='row'>
-                <img src='media/images/homeHero.png' alt='Hero Image'/>
-                <h1>Invest in everything</h1>
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5'/>
+                <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button>Signup Now</button>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin:"0 auto"}}>Sign up for free</button>
             </div>
         </div>
       );
