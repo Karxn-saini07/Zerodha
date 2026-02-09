495dcf484bf79bc63d7c4fa2a1c498953114e402
Karan <karxnsaini07@gmail.com>
2026-02-09 22:24:53 +0530

    new one

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -12,8 +12,8 @@ import Footer from '../Footer';
 function HomePage() {
     return (
         <>
-            {/* <Navbar />
-            <Hero /> */}
+            <Navbar />
+            <Hero />
             <Awards />
             <Stats />
             <Pricing />
