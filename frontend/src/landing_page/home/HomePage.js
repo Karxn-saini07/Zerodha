aff7b5f5d5e9a68d66c56f9e1e32f05618ae4dff
Karan <karxnsaini07@gmail.com>
2026-02-12 22:14:43 +0530

    all set

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Hero from './Hero';
 import Awards from './Awards';
-import Stats from './Stats';
-import Pricing from './Pricing';
+// import Stats from './Stats';
+// import Pricing from './Pricing';
 import Education from './Education';
 
 import OpenAccount from '../OpenAccount';
