fc8c210b5222980be410cd1f23e3a2e6331a6cbf
Karan <karxnsaini07@gmail.com>
2026-02-15 23:38:54 +0530

    add some codes

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -8,10 +8,8 @@ import Footer from '../Footer';
 function PricingPage() {
     return (
         <>
-            <Navbar />
             <Hero />
             <Brokerage />
-            <Footer />
         </>
     );
 }
