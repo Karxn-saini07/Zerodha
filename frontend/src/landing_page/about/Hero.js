8ef732fa1c8dd8f9579220b845e406e43ec0340c
Karan <karxnsaini07@gmail.com>
2026-02-23 23:31:16 +0530

    new line add

--- frontend/src/landing_page/about/Hero.js
+++ frontend/src/landing_page/about/Hero.js
@@ -10,8 +10,11 @@ function Hero() {
         </h1>
       </div>
 
-      <div className="row p-5 mt-5 border-top">
-        <div className="col-6">
+      <div
+        className="row p-5 mt-5 border-top text-muted"
+        style={{ lineHeight: "1.8", fontSize: "1.2rem" }}
+      >
+        <div className="col-6 p-5">
           <p>
             We kick-started operations on the 15th of August, 2010 with the goal
             of breaking all barriers that traders and investors face in India in
@@ -31,16 +34,24 @@ function Hero() {
             15% of all Indian retail trading volumes.
           </p>
         </div>
-        <div className="col-6">
-          <p>In addition, we run a number of popular open online educational and
-          community initiatives to empower retail traders and investors.</p>
+        <div className="col-6 p-5">
+          <p>
+            In addition, we run a number of popular open online educational and
+            community initiatives to empower retail traders and investors.
+          </p>
 
-          <p>Rainmatter, our fintech fund and incubator, has invested in several
-          fintech startups with the goal of growing the Indian capital markets.</p>
+          <p>
+            <a href="" style={{ textDecoration:"none" }}>Rainmatter</a>, our fintech fund and incubator, has
+            invested in several fintech startups with the goal of growing the
+            Indian capital markets.
+          </p>
 
-          <p>And yet, we are always up to something new every day. Catch up on the
-          latest updates on our blog or see what the media is saying about us or
-          learn more about our business and product philosophies.</p>
+          <p>
+            And yet, we are always up to something new every day. Catch up on
+            the latest updates on our <a href="" style={{ textDecoration:"none" }}>blog</a> or see what the media
+            is <a href="" style={{ textDecoration:"none" }}>saying about us</a> or learn more about our business
+            and product <a href="" style={{ textDecoration:"none" }}>philosophies</a>.
+          </p>
         </div>
       </div>
     </div>
